5beb3bfee058bcd7c1b8e8deeb57c49acec8835b
Varun <varunsingh7004@gmail.com>
2025-05-06 14:33:20 +0530

    font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
+import Header from "./component/Header";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
   );
 }
 
