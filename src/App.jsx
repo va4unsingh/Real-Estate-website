fa64bd9b8f91dbc1db5575b6bb6a73740b3b808b
Varun <varunsingh7004@gmail.com>
2025-05-06 17:18:44 +0530

    toaster mid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Contact } from "lucide-react";
 import "./App.css";
 import About from "./component/About";
-
 import Header from "./component/Header";
 import Projects from "./component/Projects";
 import Testimonials from "./component/Testimonials";
+import Contact from "./component/Contact";
+import { ToastContainer } from 'react-toastify';
+// import "react-toastify/dist/ReactToastify.css"
 
 function App() {
+  
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer/>
       <Header />
       <About />
       <Projects />
--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "f098b145-de75-4bf5-a4d7-761181cdd6bb");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      // setResult("Form Submitted Successfully");
+      toast.success(" Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      toast.error(data.message);
+      // setResult(data.message);
+    }
+  };
   return (
     <div
       className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden
@@ -14,8 +41,48 @@ function Contact() {
         </span>
       </h1>
       <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
-        Real Stories From Those Who Found Home With Us
+        Ready to make a Move? Let's Build Your Future Together
       </p>
+
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto text-gray-600 pt-8 "
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              placeholder="Your Name"
+              name="Name"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className="w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              placeholder="Your Email"
+              name="Email "
+              required
+            />
+          </div>
+        </div>
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none"
+            name="Message"
+            placeholder="Message"
+            required
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
     </div>
   );
 }
