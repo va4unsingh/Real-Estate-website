de04d44bf1629a191a778f3fa88b3efdb2dfdb35
Varun <varunsingh7004@gmail.com>
2025-05-06 16:01:38 +0530

    projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { assets, projectsData } from "../assets/assets";
 
 function Projects() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+  useEffect(() => {
+    const updateCardsToShow = () => {
+      if (window.innerWidth >= 1024) {
+        setCardsToShow(projectsData.length);
+      } else {
+        setCardsToShow(1);
+      }
+    };
+    updateCardsToShow();
+
+    window.addEventListener("resize", updateCardsToShow);
+    return () => window.removeEventListener("resize", updateCardsToShow);
+  }, []);
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
   return (
     <div
       className="container mx-auto py-4 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
@@ -17,6 +42,54 @@ function Projects() {
         Crafting Spaces, Building Legacies-Explore Our Portfolio
       </p>
       {/* Slider Buttons */}
+      <div className="flex justify-end items-center mb-8">
+        <button
+          onClick={prevProject}
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Previous Projects"
+        >
+          <img src={assets.left_arrow} alt="Previous" />
+        </button>
+        <button
+          onClick={nextProject}
+          className="p-3 bg-gray-200 rounded mr-2"
+          aria-label="Next Projects"
+        >
+          <img src={assets.right_arrow} alt="Next" />
+        </button>
+      </div>
+      {/* Projects Slider Container  */}
+      <div className="overflow-hidden">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`,
+          }}
+        >
+          {projectsData.map((project) => (
+            <div
+              key={project.id}
+              className="relative flex-shrink-0 w-full sm:w-1/4"
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span></span> {project.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
